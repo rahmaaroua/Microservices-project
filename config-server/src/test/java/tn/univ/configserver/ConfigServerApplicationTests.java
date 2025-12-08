16e53e7698eca10529c56eb6972a743b5ebcce59
Rahma Aroua <rahma.aroua@etudiant-fst.utm.tn>
2025-12-08 16:33:50 +0100

    first commit

---
+++ config-server/src/test/java/tn/univ/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.univ.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}*/
