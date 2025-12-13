a9dd2365dc5b15e13f72f2359ecb7952bdda7cda
abidnouha55@gmail.com <abidnouha55@gmail.com>
2025-12-13 09:32:23 +0100

    ajout microservice organisateur

---
+++ organisateur/src/main/java/com/tp/microservices/organisateur/OrganisateurApplication.java
@@ -0,0 +1,13 @@
+package com.tp.microservices.organisateur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganisateurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganisateurApplication.class, args);
+    }
+
+}
